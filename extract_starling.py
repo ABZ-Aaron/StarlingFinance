e60d31078cc8c14d3898101caf7f421003914b53
Aaron Wright <aaronjohnwright@gmail.com>
2022-06-06 23:11:32 +0100

    Updated

--- extract_starling.py
+++ extract_starling.py
@@ -3,6 +3,7 @@ import configparser
 import gspread
 import os
 from oauth2client.service_account import ServiceAccountCredentials
+from datetime import datetime
 
 """
 Application extracts total balance from Starling Bank account, and copies it to
@@ -10,7 +11,7 @@ Google Sheets cell.
 
 Pre-requisites:
 
-1. Create Google Sheet
+1. Create Google Worksheet and rename Sheet1 to Data_Assets
 2. Go to Google Developer Console
 3. Create & Select Project
 4. Go to APIs and Services and activate Google Sheets API
@@ -22,25 +23,33 @@ Pre-requisites:
 10. Go to your Google Sheet and share with this email
 11. Create Starling Developer account and create personal access token (bearer)
 12. Create file called config.conf in this directory
-12. Copy the below, replacing with your starling credentials
+13. Copy the below, replacing with your starling credentials
 
 [starling_config]
 account_id = enter account id here
 bearer = enter bearer token here
 
 """
+# MAKE CHANGES HERE
+WORKSHEET_NAME = 'Finance' # Name of your worksheet
+CELL = 'D3' # Cell you want total effective balance written to
+
 # Current path of file
 cwd = os.path.dirname(os.path.abspath(__file__))
 
 # Config data for Starling
 config = configparser.ConfigParser()
-config.read(f'{cwd}/config.conf')
+config_file = f'{cwd}/config.conf'
+config.read(config_file)
 account_id = config['starling_config']['account_id']
 bearer = config['starling_config']['bearer']
 
 # Config data from Google
-scope = ['https://www.googleapis.com/auth/spreadsheets', 'https://www.googleapis.com/auth/drive']
-credentials_google = ServiceAccountCredentials.from_json_keyfile_name(f"{cwd}/google_auth.json", scope)
+scope = ['https://www.googleapis.com/auth/spreadsheets', 
+         'https://www.googleapis.com/auth/drive']
+
+json_file = f"{cwd}/google_auth.json"
+credentials_google = ServiceAccountCredentials.from_json_keyfile_name(json_file, scope)
 file = gspread.authorize(credentials_google) 
 
 # Starling API String
@@ -57,12 +66,10 @@ r_json = r.json()
 total = r_json['totalEffectiveBalance']['minorUnits']
 total = total / 100
 
-# Update Google sheet
-sheet = file.open("Finance").sheet1
-sheet.update_acell('C3', total)
+# Update Google sheet on first worksheet
+sheet = file.open(WORKSHEET_NAME).worksheet("Data_Assets")
+sheet.update_acell(CELL, total)
 
-from datetime import datetime
-
-today = str(datetime.now())
-sheet.update_acell('B16', today)
-
+# Update Google sheet to display time script was last run
+time = str(datetime.now())
+sheet.update_acell('E3', time)
